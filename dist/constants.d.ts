70c06504e13ca364320fa77e837678bcf10d3379
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-03 18:36:49 -0800

    propagate working paradigm from beaten path

---
+++ dist/constants.d.ts
@@ -0,0 +1,4 @@
+import { Scalar } from '@musical-patterns/utilities';
+declare const PLAYROOM_TEST_PATTERN_DURATION_SCALAR: Scalar;
+declare const PLAYROOM_TEST_PATTERN_PITCH_SCALAR: Scalar;
+export { PLAYROOM_TEST_PATTERN_DURATION_SCALAR, PLAYROOM_TEST_PATTERN_PITCH_SCALAR, };
---
+++ dist/patterns.d.ts
@@ -0,0 +1,5 @@
+import { PatternSpec } from '@musical-patterns/pattern';
+import { Pattern } from '@musical-patterns/registry';
+declare const spec: PatternSpec;
+declare const pattern: Pattern;
+export { pattern, spec, };
