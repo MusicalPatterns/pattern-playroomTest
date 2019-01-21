feed16c17a5710dd1b9171c7a56989bea30087fc
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-20 17:20:13 -0800

    1.0.91: nosnapshothack

--- src/metadata/posts.ts
+++ src/metadata/posts.ts
@@ -1,7 +1,11 @@
 // tslint:disable
 
-const post: string = process.env.NODE_ENV === 'test' ? '' : require('./post.html')
-const nonPost: string = process.env.NODE_ENV === 'test' ? '' : require('./nonPost.html')
+import { Maybe } from '@musical-patterns/utilities'
+
+const env: Maybe<string> = process.env.NODE_ENV
+
+const post: string = env === 'development' || env === 'production' ? require('./post.html') : ''
+const nonPost: string = env === 'development' || env === 'production' ? require('./nonPost.html') : ''
 
 export {
     post,
