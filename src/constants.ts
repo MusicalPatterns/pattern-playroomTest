d571395f51382772bbd37c7755b434ea6715d6aa
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-31 16:58:41 -0800

    1.0.118: exclude min and maxes to test that - also have to stretch max by 1

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ import { Frequency, Milliseconds, Scalar, to } from '@musical-patterns/utilities
 
 const PLAYROOM_TEST_INITIAL_BASE_DURATION: Milliseconds = to.Milliseconds(100)
 const PLAYROOM_TEST_INITIAL_BASE_FREQUENCY: Frequency = to.Frequency(4186)
-const PLAYROOM_TEST_MAX_BASE_DURATION: Milliseconds = to.Milliseconds(1002)
+const PLAYROOM_TEST_MAX_BASE_DURATION: Milliseconds = to.Milliseconds(1003)
 const PLAYROOM_TEST_MIN_BASE_DURATION: Milliseconds = to.Milliseconds(9)
 
 export {
