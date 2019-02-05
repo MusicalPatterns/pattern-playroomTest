eb6b8a1c535fe55ebc787f35eb3ab312563add24
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-05 10:50:03 -0800

    1.0.121: scientific pitch

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,18 @@
 // tslint:disable no-magic-numbers
 
-import { Frequency, Milliseconds, Scalar, to } from '@musical-patterns/utilities'
+import {
+    Frequency, from,
+    Milliseconds,
+    SCIENTIFIC_PITCHES,
+    ScientificPitchNoteName,
+    ScientificPitchOctaveNumber,
+    to,
+} from '@musical-patterns/utilities'
 
 const PLAYROOM_TEST_INITIAL_BASE_DURATION: Milliseconds = to.Milliseconds(100)
-const PLAYROOM_TEST_INITIAL_BASE_FREQUENCY: Frequency = to.Frequency(4186)
+const PLAYROOM_TEST_INITIAL_BASE_FREQUENCY: Frequency = to.Frequency(Math.round(from.Frequency(
+    SCIENTIFIC_PITCHES[ ScientificPitchNoteName.C ][ ScientificPitchOctaveNumber._8 ],
+)))
 const PLAYROOM_TEST_MAX_BASE_DURATION: Milliseconds = to.Milliseconds(1003)
 const PLAYROOM_TEST_MIN_BASE_DURATION: Milliseconds = to.Milliseconds(9)
 
