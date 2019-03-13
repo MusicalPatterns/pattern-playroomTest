3cdb9571535f4f8bbf1bdf56addc36976d99cf45
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-12 23:36:14 -0700

    1.0.173: initial is now initialSpecs - had been back and forth on it but now that there is a difference between spec and specs this definitely is the right way to go

--- src/spec/configurations.ts
+++ src/spec/configurations.ts
@@ -55,18 +55,17 @@ const configurations: PlayroomTestConfigurations = {
 }
 
 const differentConfigurations: PlayroomTestConfigurations = {
+    ...standardConfigurations,
     ...configurations,
     [ StandardSpec.BASE_FREQUENCY ]: {
-        ...configurations[ StandardSpec.BASE_FREQUENCY ],
+        ...standardConfigurations[ StandardSpec.BASE_FREQUENCY ],
         hideInput: RangedInputType.NUMBER,
     },
     [ PlayroomTestSpec.ARRAYED_SPEC ]: {
+        ...configurations[ PlayroomTestSpec.ARRAYED_SPEC ],
         constraint: {
             min: 0,
         },
-        formattedName: 'example arrayed control',
-        inputType: InputType.RANGED,
-        isArrayed: true,
     },
 }
 
