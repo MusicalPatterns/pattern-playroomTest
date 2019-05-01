159953e3f0987d595b5dbdb2732bd641ef821674
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-05-01 15:25:05 -0700

    1.0.224: follow up to scales are objects

--- src/material/notes.ts
+++ src/material/notes.ts
@@ -1,10 +1,10 @@
-import { Note, STANDARD_PITCH_SCALE_INDEX, STANDARD_VALUE_SCALE_INDEX } from '@musical-patterns/material'
+import { Note } from '@musical-patterns/material'
 import { PLAYROOM_TEST_NOTE_PITCH_SCALAR, PLAYROOM_TEST_NOTE_VALUE_SCALAR } from './constants'
 
 const computeNotes: () => Note[] =
     (): Note[] => [ {
-        pitch: { scalar: PLAYROOM_TEST_NOTE_PITCH_SCALAR, scaleIndex: STANDARD_PITCH_SCALE_INDEX },
-        value: { scalar: PLAYROOM_TEST_NOTE_VALUE_SCALAR, scaleIndex: STANDARD_VALUE_SCALE_INDEX },
+        pitch: { scalar: PLAYROOM_TEST_NOTE_PITCH_SCALAR },
+        value: { scalar: PLAYROOM_TEST_NOTE_VALUE_SCALAR },
     } ]
 
 export {
