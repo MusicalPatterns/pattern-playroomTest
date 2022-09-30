d394858eef470da656f060ac93663534f847f19b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2022-09-29 18:38:14 -0700

    1.0.234: deal with change in html-loader

--- src/metadata/posts.ts
+++ src/metadata/posts.ts
@@ -4,7 +4,7 @@ import { Maybe } from '@musical-patterns/utilities'
 
 const env: Maybe<string> = process.env.NODE_ENV
 
-const post: string = env !== 'test' && env != 'material' ? require('./post.html') : ''
+const post: string = env !== 'test' && env != 'material' ? require('./post.html').default : ''
 const nonPost: string = env === 'development' || env === 'production' ? require('./nonPost.html') : ''
 
 export {
